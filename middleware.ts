ab219eff6f7d21bf9da7532848f45dec453511bc
Shreyash Gautam <shreyashgautam2007@gmail.com>
2025-05-17 00:41:25 +0530

    change in middleware

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,9 @@ export default clerkMiddleware();
 export const config = {
   matcher: [
     "/",
-    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon.ico).*)", // Protect all except static assets
-    "/api/webhooks/clerk",  // Exclude this webhook route from auth protection by removing it from matcher
-    "/api/:path*",          // Protect all other API routes
-    "/trpc/:path*",         // Protect tRPC routes
-  ].filter(path => path !== "/api/webhooks/clerk"), // Remove webhook route from matcher to make it public
+    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon.ico).*)",
+    "/api/:path*",
+    "/trpc/:path*",
+  ],
 };
+
